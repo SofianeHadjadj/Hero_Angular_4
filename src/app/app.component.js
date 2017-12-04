7e62b36d281eef9142dd4e6aa92d6d9050521855
Hadjadj Sofiane <sofiane.hadjadj.etu@unilim.fr>
2017-12-04 13:53:08 +0100

    4. Multiple Components

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -6,12 +6,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var Hero = (function () {
-    function Hero() {
-    }
-    return Hero;
-}());
-exports.Hero = Hero;
 var HEROES = [
     { id: 1, name: 'Wolverine' },
     { id: 2, name: 'Hulk' },
@@ -38,7 +32,7 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n  \t\t<h1>{{title}}</h1>\n  \t\t<h2> My Heroes</h2>\n  \t\t<ul class=\"heroes\">\n\t\t\t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n  \t\t</ul>\n  \t\t<div *ngIf=\"selectedHero\">\n\t  \t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t\t<div>\n\t\t\t    <label>name: </label>\n\t\t\t    <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n\t\t\t</div>\n\t\t</div>\n  \t\t",
+        template: "\n  \t\t<h1>{{title}}</h1>\n  \t\t<h2> My Heroes</h2>\n  \t\t<ul class=\"heroes\">\n\t\t\t<li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n\t\t\t\t<span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t\t\t</li>\n  \t\t</ul>\n  \t\t<!--<div *ngIf=\"selectedHero\">\n\t  \t\t<h2>{{selectedHero.name}} details!</h2>\n\t\t\t<div><label>id: </label>{{selectedHero.id}}</div>\n\t\t\t<div>\n\t\t\t    <label>name: </label>\n\t\t\t    <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n\t\t\t</div>\n\t\t</div>-->\n\t\t<hero-detail [hero]=\"selectedHero\"></hero-detail>\n  \t\t",
         styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 2px;\n  }\n  .heroes li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 2px 0 0 2px;\n  }\n"]
     })
 ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from 'hero-detail/hero';
 
 const HEROES: Hero[] = [
   { id: 1, name: 'Wolverine' },
@@ -28,14 +24,15 @@ const HEROES: Hero[] = [
 				<span class="badge">{{hero.id}}</span> {{hero.name}}
 			</li>
   		</ul>
-  		<div *ngIf="selectedHero">
+  		<!--<div *ngIf="selectedHero">
 	  		<h2>{{selectedHero.name}} details!</h2>
 			<div><label>id: </label>{{selectedHero.id}}</div>
 			<div>
 			    <label>name: </label>
 			    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
 			</div>
-		</div>
+		</div>-->
+		<hero-detail [hero]="selectedHero"></hero-detail>
   		`,
   styles: [`
   .selected {
@@ -93,7 +90,7 @@ export class AppComponent  {
 	title = 'Tour of Heroes';
 	heroes = HEROES;
 	selectedHero : Hero;
-	
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
